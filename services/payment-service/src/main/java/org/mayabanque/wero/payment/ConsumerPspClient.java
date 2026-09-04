b835313c60cded9cd44b122b5ac689511a63e522
zidane <zidane.djamal@gmail.com>
2026-09-04 21:49:07 +0200

    V1 add Consumer PSP REST client

---
+++ services/payment-service/src/main/java/org/mayabanque/wero/payment/ConsumerPspClient.java
@@ -0,0 +1,17 @@
+package org.mayabanque.wero.payment;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@Path("/consumer/payments")
+@RegisterRestClient(configKey = "consumer-psp")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public interface ConsumerPspClient {
+    @POST
+    PaymentResource.PaymentResponse pay(PaymentResource.PaymentRequest request);
+}
