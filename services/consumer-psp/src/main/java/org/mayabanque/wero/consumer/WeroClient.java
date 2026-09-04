d1782a3d46f79bdc871b9b90b01980542698487c
zidane <zidane.djamal@gmail.com>
2026-09-04 21:49:41 +0200

    V1 add Wero mock client

---
+++ services/consumer-psp/src/main/java/org/mayabanque/wero/consumer/WeroClient.java
@@ -0,0 +1,14 @@
+package org.mayabanque.wero.consumer;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@Path("/wero/payments")
+@RegisterRestClient(configKey="mock-wero")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public interface WeroClient {
+    @POST
+    ConsumerPaymentResource.PaymentResponse forward(ConsumerPaymentResource.PaymentRequest request);
+}
